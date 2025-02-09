22589c0acf7584ca9fe9f261f03db4e5955f6c14
Nina Wyckoff <2917069w@student.gla.ac.uk>
2025-02-09 16:31:43 +0000

    Restored files from commit 9b745dacd03b852d414e616fe422495ec842eb89

--- rango/admin.py
+++ rango/admin.py
@@ -5,9 +5,5 @@ class PageAdmin(admin.ModelAdmin):
 
     list_display = ('title', 'category', 'url')
 
-# Add in this class to customise the Admin Interface
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
-
-admin.site.register(Category, CategoryAdmin)
+admin.site.register(Category)
 admin.site.register(Page, PageAdmin)
