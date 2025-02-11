098811b743e9880e9e047cf3bc407e6be0b62171
Nina Wyckoff <2917069w@student.gla.ac.uk>
2025-02-11 17:22:36 +0000

    Chapet seven exercises complete'

--- rango/forms.py
+++ rango/forms.py
@@ -8,6 +8,18 @@ class CategoryForm(forms.ModelForm):
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+       
+        # If url is not empty and doesn't start with 'http://',
+        # then prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+            
+        return cleaned_data
+
     class Meta:
         model = Category
         fields = ('name',)
